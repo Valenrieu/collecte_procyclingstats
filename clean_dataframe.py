9aec026b1561a1d5bacba17260202beda5aac587
Valenrieu <rieuvalentin3@gmail.com>
2024-01-12 15:48:56 +0100

    modif

--- clean_dataframe.py
+++ clean_dataframe.py
@@ -3,7 +3,11 @@
 import pandas as pd
 
 def clean_races_file():
-    df = pd.read_csv("out/courses2023.csv", encoding="utf-8")
+    listt = ["premier", "deuxieme", "troisieme", "quatrieme", "cinquieme",
+             "sixieme", "septieme", "huitieme", "neuvieme", "dixieme"]
+
+    df = pd.read_csv("out/courses2023.csv", encoding="utf-8", dtype={"rang_course":"Int32",
+                     "score_denivele":"Int32", "score_qualite_ligne_depart":"Int32"})
 
     for row in range(len(df)):
         try:
@@ -21,6 +25,18 @@ def clean_races_file():
 
         except AttributeError:
             pass
+
+        for col in listt:
+            if pd.isna(df.loc[row, col]):
+                pass
+
+            else:
+                temp = df.loc[row, col].split(" ")
+
+                for item in range(len(temp)):
+                    temp[item] = temp[item].capitalize()
+
+                df.loc[row, col] = " ".join(temp)
 
         try:
             if df.loc[row, "distance"]=="-":
@@ -45,6 +61,7 @@ def clean_races_file():
         if df.loc[row, "maniere_gagner"]=="? - let us know!":
             df.loc[row, "maniere_gagner"] = float("nan")
 
+    df.temperature.astype("Int16")
     df.drop(columns=["type_parcours"], axis=1, inplace=True)
     df.to_csv("out/courses2023.csv", encoding="utf-8", index=False)
 
